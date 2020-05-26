8ab380931a3a2c3740497f9c8e2a48132fdfed64
Arnaud Banvillet <arnaud@matierenoire.io>
2020-05-26 17:26:17 +0200

    Event’s card that closed in the panel

--- src/js/eventDate/DateDetails.js
+++ src/js/eventDate/DateDetails.js
@@ -1,35 +1,44 @@
 import { dateFormat, timeFormat } from '../utils'
 const { useState } = wp.element
 const { __, sprintf } = wp.i18n
-const { PanelRow, Modal, Button, Card, CardHeader, CardBody, CardFooter } = wp.components
+const { PanelRow, Modal, Button, Card, CardHeader, CardBody, CardFooter, Icon } = wp.components
+import { chevronUp, chevronDown } from '@wordpress/icons'
 
 const DateDetails = ({ date, editDate, deleteDate }) => {
   const [confirmOpen, setConfirmOpen] = useState(false)
+  const [bodyOpen, setBodyOpen] = useState(true)
 
   return (
     <PanelRow>
       <Card size="extraSmall" style={{ width: '100%' }}>
-        <CardHeader>
-          <strong>{date.startDate.toLocaleDateString()}</strong>
+        <CardHeader style={{ padding: 0 }}>
+          <Button onClick={() => setBodyOpen(!bodyOpen)} style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+            {date.startDate.toLocaleDateString()}
+            <Icon className="components-panel__arrow" icon={bodyOpen ? chevronUp : chevronDown} />
+          </Button>
         </CardHeader>
-        <CardBody>
-          <PanelRow>
-            <span>{__('Start')}</span>
-            <span>{dateFormat(date.startDate)}</span>
-          </PanelRow>
-          <PanelRow>
-            <span>{__('End')}</span>
-            <span>{dateFormat(date.endDate)}</span>
-          </PanelRow>
-        </CardBody>
-        <CardFooter style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Button isSecondary onClick={() => editDate(date)}>
-            {__('Edit')}
-          </Button>
-          <Button isDestructive onClick={() => setConfirmOpen(true)}>
-            {__('Delete')}
-          </Button>
-        </CardFooter>
+        {bodyOpen && (
+          <>
+            <CardBody>
+              <PanelRow>
+                <span>{__('Start')}</span>
+                <span>{dateFormat(date.startDate)}</span>
+              </PanelRow>
+              <PanelRow>
+                <span>{__('End')}</span>
+                <span>{dateFormat(date.endDate)}</span>
+              </PanelRow>
+            </CardBody>
+            <CardFooter style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button isSecondary onClick={() => editDate(date)}>
+                {__('Edit')}
+              </Button>
+              <Button isDestructive onClick={() => setConfirmOpen(true)}>
+                {__('Delete')}
+              </Button>
+            </CardFooter>
+          </>
+        )}
       </Card>
       {confirmOpen && (
         <Modal title={__('Delete date')} onRequestClose={() => setConfirmOpen(false)}>
